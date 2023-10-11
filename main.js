55121cb94dd2ff61fd96b5b6e561ea8f8def4401
Bexruz <141207743+CrayzeS07@users.noreply.github.com>
2023-10-11 18:39:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// 1 - masala
+var Uz = + prompt('son kiriting');
+for (i=1;i<=Uz;i++){
+    alert(i);
+}
+
+//3 - masala
+var Uz1 = + prompt('son kiriting');
+var  v =0;
+
+    for(i=1;i<=Uz1;i++){
+        v +=i;
+    }
+    alert(v);
+
+    //4 - masala    
+    var n = + prompt('Son Kiriting');
+    var v = 0;
+
+    for(i=2;i<=n;i+=2){
+        v += i;
+        console.log(i);
+    }
+    console.log(v);
